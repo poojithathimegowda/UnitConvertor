8a292602658c9771e9760a05debdceed43e0d55a
poojithathimegowda <poojithagowda98@gmail.com>
2024-08-30 15:06:54 +0200

    added the logic cs file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,25 +20,111 @@ namespace UnitConvertor
     /// </summary>
     public partial class MainWindow : Window
     {
-        double inputNo,outputNo;
-        string convertionType;
-
-       
-
-        private void ConvertButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void convertFrom_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
+        double inputValue;
         public MainWindow()
         {
             InitializeComponent();
-            input.Text= "0";
+
+        }
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (double.TryParse(InputValue.Text, out inputValue))
+            {
+                string fromUnit = (FromUnit.SelectedItem as ComboBoxItem)?.Content.ToString();
+                string toUnit = (ToUnit.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+                double conversionFactor = GetConversionFactor(fromUnit, toUnit);
+
+                double result = inputValue * conversionFactor;
+                Result.Text = $"{inputValue} {fromUnit} = {result} {toUnit}";
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+
+        private double GetConversionFactor(string fromUnit, string toUnit)
+        {
+          
+            double factor = 1.0;
+
+            switch (fromUnit)
+            {
+                case "Meters":
+                    switch (toUnit)
+                    {
+                        case "Meters":
+                            factor = 1.0;
+                            break;
+                        case "Kilometers":
+                            factor = 0.001;
+                            break;
+                        case "Feet":
+                            factor = 3.28084;
+                            break;
+                        case "Miles":
+                            factor = 0.000621371;
+                            break;
+                    }
+                    break;
+                case "Kilometers":
+                    switch (toUnit)
+                    {
+                        case "Meters":
+                            factor = 1000.0;
+                            break;
+                        case "Kilometers":
+                            factor = 1.0;
+                            break;
+                        case "Feet":
+                            factor = 3280.84;
+                            break;
+                        case "Miles":
+                            factor = 0.621371;
+                            break;
+                    }
+                    break;
+                case "Feet":
+                    switch (toUnit)
+                    {
+                        case "Meters":
+                            factor = 0.3048;
+                            break;
+                        case "Kilometers":
+                            factor = 0.0003048;
+                            break;
+                        case "Feet":
+                            factor = 1.0;
+                            break;
+                        case "Miles":
+                            factor = 0.000189394;
+                            break;
+                    }
+                    break;
+                case "Miles":
+                    switch (toUnit)
+                    {
+                        case "Meters":
+                            factor = 1609.34;
+                            break;
+                        case "Kilometers":
+                            factor = 1.60934;
+                            break;
+                        case "Feet":
+                            factor = 5280.0;
+                            break;
+                        case "Miles":
+                            factor = 1.0;
+                            break;
+                    }
+                    break;
+            }
+
+            return factor;
         }
     }
+
 }
